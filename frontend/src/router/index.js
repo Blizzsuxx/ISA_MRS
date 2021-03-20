480af5d3e6e61f7b508c4de4b6bfbbe6d9ad6cdf
vAndrija <andrija.vojnovic@uns.ac.rs>
2021-03-20 21:22:20 +0100

    dodat element-plus

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,14 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import About from '../views/About'
 
 const routes = [
   
-  {
-    path: '/about',
-    name: 'About',
-    component: About,
-  },
+  
 ]
 
 const router = createRouter({
