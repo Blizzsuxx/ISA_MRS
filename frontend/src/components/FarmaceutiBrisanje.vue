06b862e8ded64df96c278fbf69fb4c91e5d9fc98
vAndrija <andrija.vojnovic@uns.ac.rs>
2021-05-05 18:16:17 +0200

    dodata tabela za prikaz dermatologa u apoteci

---
+++ frontend/src/components/FarmaceutiBrisanje.vue
@@ -0,0 +1,81 @@
+<template>
+  <NavAdminApoteke />
+  <el-row :gutter="20">
+    <el-col :span="24">
+      <h5>Dermatolozi</h5>
+      <DermatoloziTabela
+        @promjenjena-selekcija="promjenaDermatologa"
+        v-bind:dermatolozi="$store.state.Dermatolozi.sviDermatolozi"
+    /></el-col>
+    </el-row>
+    <el-row :gutter="20">
+      <el-col :span="24">
+    <el-popconfirm title="Da li ste sigurni da zelite da otpustitite farmaceuta?" @confirm="potvrdjeno" confirmButtonText='OK'
+  cancelButtonText='Odustani'>
+      <template #reference>
+        <el-button type="danger" plain>Delete</el-button>
+      </template>
+    </el-popconfirm>
+    </el-col>
+  </el-row>
+</template>
+<style scoped>
+.item {
+  margin-bottom: 18px;
+}
+
+.box-card {
+  width: 600px;
+}
+.poravnanje {
+  align-content: center;
+  align-items: center;
+  text-align: center;
+}
+#app {
+  position: relative;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
+
+<script>
+import NavAdminApoteke from "./NavAdminApoteke";
+import DermatoloziTabela from "./DermatoloziTabela";
+import { mapState } from "vuex";
+export default {
+  name: "FarmaceutiBrisanje",
+  props: { options: String },
+  data() {
+    return {
+      dermatolog : null
+    }
+  },
+  methods: {
+    promjenaDermatologa (val) {
+     
+      this.dermatolog = val
+      
+    },
+    potvrdjeno(){
+     
+       this.$store.dispatch("Farmaceuti/otpustiFarmaceuta",this.dermatolog.id).then(()=>{
+           this.$store.dispatch("Farmaceuti/dobaviFarmaceuteAdmin");
+       })
+    }
+  },
+  components: {
+    NavAdminApoteke,
+    DermatoloziTabela,
+  },
+  mounted() {
+    this.$store.dispatch("Farmaceuti/dobaviFarmaceuteAdmin");
+  },
+
+  computed: {
+    ...mapState("APApoteke", ["apoteka"]),
+  },
+};
+</script>
--- frontend/src/store/modules/Farmaceuti.js
+++ frontend/src/store/modules/Farmaceuti.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-
+import authHeader from './AuthHeader'
 const state = {
     sviFarmaceuti :[],
     
@@ -17,6 +17,25 @@ const actions = {
             })
 
 
+    },
+    dobaviFarmaceuteAdmin (context) {
+        axios.get('http://localhost:8080/api/v1/farmaceut/admin', { headers: authHeader()})
+            .then(response => {
+                let farmaceuti =response.data
+                context.commit('postaviFarmaceute',farmaceuti)
+            })
+
+
+    },
+    optustiFarmaceuta(context,id){
+        return axios.put(`http://localhost:8080/api/v1/farmaceut/otpustiFarmaceuta/${id}`, {},{ headers: authHeader()})
+            .then(() => {
+                
+                return context;
+                
+            })
+
+     
     },
   
 
